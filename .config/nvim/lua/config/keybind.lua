384cbad803eaca5ced43cce3e17340874ea4d44c
hirano00o <uday6827+github@gmail.com>
2026-03-03 07:50:22 +0900

    Remove toggleterm

--- .config/nvim/lua/config/keybind.lua
+++ .config/nvim/lua/config/keybind.lua
@@ -6,16 +6,6 @@ vim.keymap.set("n", "<ESC><ESC>", "<Cmd>noh<CR>", { silent = true })
 vim.keymap.set("n", "<C-l>", "<Cmd>BufferNext<CR>", { desc = "Next to right tab" })
 vim.keymap.set("n", "<C-h>", "<Cmd>BufferPrevious<CR>", { desc = "Next to left tab" })
 vim.keymap.set("n", "<leader>w", "<Cmd>BufferClose<CR>", { desc = "Close tab" })
--- termがタブとして残らないように、ターミナルはバッファから除外
-local term_augroup = vim.api.nvim_create_augroup('TerminalSettings', { clear = true })
-vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
-  group = term_augroup,
-  pattern = 'term://*',
-  callback = function(args)
-    vim.bo[args.buf].buflisted = false
-    vim.bo[args.buf].bufhidden = 'hide'
-  end,
-})
 
 -- jsonをjqでフォーマット
 vim.api.nvim_create_autocmd("FileType", {
