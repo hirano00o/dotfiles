3f28e12d601424fc474f0ee1fcbd6c431d096568
hirano00o <uday6827+github@gmail.com>
2026-04-25 14:26:09 +0900

    Fix nix configuration

--- nix-darwin/config/nix.nix
+++ nix-darwin/config/nix.nix
@@ -1,11 +1,22 @@
 {
   nix = {
     optimise.automatic = true;
+    gc = {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 2;
+        Minute = 0;
+      };
+      options = "--delete-older-than 14d";
+    };
     settings = {
       sandbox = "relaxed";
       experimental-features = "nix-command flakes";
       download-buffer-size = 268435456;
-      min-free = 1073741824; # 1 GiB
+      min-free = 5368709120; # 5 GiB
+      max-free = 10737418240; # 10 GiB
+      auto-optimise-store = true;
     };
   };
 }
