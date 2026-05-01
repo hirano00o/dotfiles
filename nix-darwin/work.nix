eba90be9fd69b92c44c3f289c70c9a8d47136442
hirano00o <uday6827+github@gmail.com>
2026-05-01 12:03:06 +0900

    Disabled the certificates symlink

--- nix-darwin/work.nix
+++ nix-darwin/work.nix
@@ -4,6 +4,8 @@
     ./default.nix
   ];
 
+  environment.etc."ssl/certs/ca-certificates.crt".enable = false;
+
   nix.settings.ssl-cert-file = "/etc/ssl/certs/ca-certificates.crt";
 
   homebrew = {
