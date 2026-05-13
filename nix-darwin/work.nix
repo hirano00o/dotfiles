d6ef07795c95fdb467187fd7ebd2267096ab9f2e
hirano00o <uday6827+github@gmail.com>
2026-05-13 16:36:57 +0900

    Remove unnecessary application and fix zsh configuration

--- nix-darwin/work.nix
+++ nix-darwin/work.nix
@@ -2,16 +2,10 @@
 {
   imports = [
     ./default.nix
+    ./config/zsh.nix
   ];
 
   environment.etc."ssl/certs/ca-certificates.crt".enable = false;
 
   nix.settings.ssl-cert-file = "/etc/ssl/certs/ca-certificates.crt";
-
-  homebrew = {
-    casks = [
-      "windows-app"
-      "microsoft-teams"
-    ];
-  };
 }
