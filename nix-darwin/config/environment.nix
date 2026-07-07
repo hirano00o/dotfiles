cedf13cf71050bdee5509ffe2e5af7e35fcede05
hirano00o <uday6827+github@gmail.com>
2026-07-08 08:06:46 +0900

    Fix because nix-darwin occurred an error

--- nix-darwin/config/environment.nix
+++ nix-darwin/config/environment.nix
@@ -3,4 +3,9 @@
   environment.variables = {
     EDITOR = "nvim";
   };
+
+  # `darwin manual html` のエラーが出るためfalseに設定
+  # nix-darwin側が修正されたら戻す
+  documentation.doc.enable = false;
+  system.tools.darwin-uninstaller.enable = false;
 }
